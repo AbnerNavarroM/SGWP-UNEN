7de71390a20a5f6cfebd7e1d89470f2d95f3a8bc
Abner Navarro <abner.navarro98@gmail.com>
2022-05-09 20:19:27 -0600

    First Commit

---
+++ dist/precache-manifest.aee573f9ac8e57488c09d06cf4355570.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bf2b52fb0a1fdf3c144051622d5ba3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "182d0974e24d5689201f",
+    "url": "/static/css/main.275dbc98.chunk.css"
+  },
+  {
+    "revision": "66dd50b51eeb5fa9b5e9",
+    "url": "/static/js/2.034f39ee.chunk.js"
+  },
+  {
+    "revision": "1411a78e7e2907f095d614daf43ccf0b",
+    "url": "/static/js/2.034f39ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "182d0974e24d5689201f",
+    "url": "/static/js/main.b7b63bf7.chunk.js"
+  },
+  {
+    "revision": "5f769730e1a726d43896",
+    "url": "/static/js/runtime-main.134f59c7.js"
+  },
+  {
+    "revision": "b4ddbf1121f52143cdf3be55c4048c65",
+    "url": "/static/media/ANECYSLOGO2.b4ddbf11.png"
+  },
+  {
+    "revision": "3e42acaab472e4cba1e143fccc29e2b5",
+    "url": "/static/media/BRANDLOGO.3e42acaa.jpg"
+  },
+  {
+    "revision": "e6e538a6703de03dae0a1c37f229b555",
+    "url": "/static/media/Maintaince.e6e538a6.png"
+  },
+  {
+    "revision": "94478d075c1c2a27fa2e493bed23c2a0",
+    "url": "/static/media/UNENLOGO2.94478d07.png"
+  },
+  {
+    "revision": "cdce1576e88355e66382643ef5455817",
+    "url": "/static/media/bg-tarjeta-02.cdce1576.jpg"
+  },
+  {
+    "revision": "6611645bd370943ebc7139d4062b9377",
+    "url": "/static/media/chip-tarjeta.6611645b.png"
+  },
+  {
+    "revision": "8ac3b5d4477a35d7c4e9f75fc077358d",
+    "url": "/static/media/gmail.8ac3b5d4.png"
+  },
+  {
+    "revision": "d2eb00b710fa2ee2c6a4408cb42f6fb4",
+    "url": "/static/media/visa.d2eb00b7.png"
+  }
+]);
